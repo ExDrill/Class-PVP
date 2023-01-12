9bb8d010544d83754c8573ab11af18900b791c86
ExDrill <72329798+ExDrill@users.noreply.github.com>
2023-01-12 12:34:25 -0500

    Add debug toggle

--- BP/scripts/misc/debug_commands.js
+++ BP/scripts/misc/debug_commands.js
@@ -1,9 +1,19 @@
 import * as RoundHandler from '../game/round.js'
 import { world } from '@minecraft/server'
 
-world.events.beforeChat.subscribe(event => {
+world.events.chat.subscribe(event => {
     const msg = event.message
     const sender = event.sender
+
+    if (sender.isOp() && msg == "toggleDebug") {
+        if (!sender.hasTag("debug")) {
+            sender.addTag("debug")
+            world.say(`${sender.name} has enabled debug mode`)
+        } else {
+            sender.removeTag("debug")
+            world.say(`${sender.name} has disabled debug mode`)
+        }
+    }
 
     if (sender.hasTag("debug")) {
         if (msg == "start") {
