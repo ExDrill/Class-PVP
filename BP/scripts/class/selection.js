9be557c9c5525a3ad68b6f7432a7a5b5b27a3299
ExDrill <72329798+ExDrill@users.noreply.github.com>
2023-01-13 11:56:38 -0500

    Class Selection Compass

--- BP/scripts/class/selection.js
+++ BP/scripts/class/selection.js
@@ -1,4 +1,5 @@
 import { world } from '@minecraft/server'
+import { ActionFormData } from '@minecraft/server-ui'
 
 export const classList = [
     "knight",
@@ -6,6 +7,28 @@ export const classList = [
     "tank",
     "gunner"
 ]
+
+world.events.beforeItemUse.subscribe(event => {
+    const item = event.item
+    const player = event.source
+    if (event.source.typeId != "minecraft:player") return
+    if (item.typeId != "minecraft:compass") return
+    
+    const classSelection = new ActionFormData()
+    classSelection.title("Choose your class!")
+    classSelection.button("Knight", "textures/items/iron_sword.png")
+    classSelection.button("Archer", "textures/items/bow_standby.png")
+    classSelection.button("Tank", "textures/items/iron_axe.png")
+    classSelection.button("Gunner", "textures/items/gunpowder.png")
+    
+    classSelection.show(player).then(data => {
+        player.triggerEvent('classpvp:reset_class')
+        if (data.canceled) {
+        } else {
+            player.triggerEvent(`classpvp:${classList[data.selection]}`)
+        }
+    })
+})
 
 world.events.beforeChat.subscribe(event => {
     const msg = event.message
